3fb53b87265dc318174cc2cd6c7d4cd663ff6adb
Richard Travis <r.z.travis@gmail.com>
2017-11-16 16:36:52 -0700

    Assignment 6 of Angular complete

--- app/scripts/services/Fixtures.js
+++ app/scripts/services/Fixtures.js
@@ -36,6 +36,14 @@
       return albumPicasso;
     };
 
+    Fixtures.getCollection = function(numberOfAlbums){
+      this.albums = [];
+      for (var i = 0; i < numberOfAlbums; i++){
+        this.albums.push(albumPicasso);
+      }
+      return this.albums;
+    };
+
     return Fixtures;
   }
 
