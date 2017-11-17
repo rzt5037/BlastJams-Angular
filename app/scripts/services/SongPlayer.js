c95b3c728be0ab8c60a3f2d4b3404e72f292e7a6
Richard Travis <r.z.travis@gmail.com>
2017-11-17 16:23:14 -0700

    Assignment 7 of angular complete

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -2,6 +2,10 @@
   function SongPlayer() {
     var SongPlayer = {};
 
+    /**
+    *@desc Currently playing audio file
+    *@type {Object}
+    */
     var currentSong = null;
 
   /**
@@ -29,16 +33,24 @@
       currentSong = song;
     }
 
+    /**
+    *@function playSong
+    *@desc Causes the currentBuzzObject to being playing audio and sets the playing value to true
+    *@param {Object} song
+    */
+    var playSong = function(song){
+      currentBuzzObject.play();
+      song.playing = true;
+    }
+
     SongPlayer.play = function(song){
       if(currentSong !== song){
         setSong(song);
-        currentBuzzObject.play();
-        song.playing = true;
+        playSong(song);
       }
       else if(currentSong === song){
         if(currentBuzzObject.isPaused()){
-          currentBuzzObject.play();
-          song.playing = true;
+          playSong(song);
         }
       }
     };
