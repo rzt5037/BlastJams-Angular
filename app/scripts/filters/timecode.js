5c66f2b5920a2d30e7d4469c02eba5197331e94f
Richard Travis <r.z.travis@gmail.com>
2017-11-22 13:42:12 -0700

    Updated timecode to use toTime

--- app/scripts/filters/timecode.js
+++ app/scripts/filters/timecode.js
@@ -1,7 +1,7 @@
 (function(){
   function timecode(){
     return function(seconds){
-      var seconds = Number.parseFloat(seconds);
+      /*var seconds = Number.parseFloat(seconds);
 
       if (Number.isNaN(seconds)){
         return '-:--';
@@ -17,7 +17,9 @@
         output += '0';
       }
 
-      output += remainingSeconds;
+      output += remainingSeconds;*/
+
+      output = buzz.toTimer(seconds);
 
       return output;
     };
